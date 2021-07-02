557962fbe365d97496ecd1e383701834150bc8a8
Icey <73022629+IceBearBots@users.noreply.github.com>
2021-07-02 18:51:15 +0800

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
-document.oncontextmenu = function(e){
+window.oncontextmenu = function() {
     return false;
+}
+window.onload = function() {
+    console.log(new Date + ': Page fully loaded.')
+    var navshare = document.getElementById('navshare');
+    navshare.addEventListener('click', () => {
+        try {
+            navigator.share();
+        } catch (err) {
+            alert("Unable to share Site. Just try copying the link manually instead.")
+            console.log(new Date + ': Error encountered.\n' + err);
+            }
+    })
 }
