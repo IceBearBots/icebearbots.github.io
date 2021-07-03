4488b85623677e031fcaf4427d3dad1bd23fb065
Icey <73022629+IceBearBots@users.noreply.github.com>
2021-07-03 14:11:34 +0800

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,16 @@ window.onload = function() {
             })
         } catch (err) {
             alert("Unable to share Site. Just try copying the link manually instead.")
-            console.log(new Date + ': Error encountered.\n' + err);
+            console.log(new Date + ': Error encountered while sharing Site.\n' + err);
+        }
+    })
+    var privacyalert = document.getElementById('privacyopen');
+    privacyalert.addEventListener('click', () => {
+        try {
+            alert(`We don't collect any data. We don't even have a database to store stuff.\nIf NSA comes around asking, we have nothing to give them, period.`)
+        } catch(err) {
+            document.write(`We don't collect any data. We don't even have a database to store stuff.<br>If NSA comes around asking, we have nothing to give them, period.`)
+            console.log(new Date + ': Error encountered while displaying Privacy Policy.\n' + err) 
         }
     })
 }
