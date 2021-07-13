747eae714aa9bee2cf5d1aedee8617e0beb0d8c8
Icey <73022629+IceBearBots@users.noreply.github.com>
2021-07-13 19:48:48 +0800

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,11 @@
 window.addEventListener('load', () => {
-    console.log(new Date + ': Page fully loaded.')
+    console.log(new Date + ': Page fully loaded.');
     var navshare = document.getElementById('navshare');
     var copyurl = location.href;
     navshare.addEventListener('click', () => {
         try {
             navigator.share({
-                title: "Icey's Site",
-                url: copyurl
+                url: copyurl,
             })
             console.log(new Date + ': Share menu has loaded.')
         } catch (err) {
@@ -23,30 +22,36 @@ window.addEventListener('load', () => {
     var menuvisible = false;
     var openlink = document.getElementById('openlink');
     var sourcecode = document.getElementById('sourcecode');
-    var copylinkoption = document.getElementById('copylink');
+    var sharelinkoption = document.getElementById('sharelink');
     var rawimgvid = document.getElementById('rawimgvid');
     var copytext = document.getElementById('copytext')
+    window.addEventListener("dblclick", e => {
+        if(e.target.tagName === "IMG" || e.target.tagName === "VIDEO") {
+            location.href = e.target.src;
+        }
+    })
     window.addEventListener("contextmenu", e => {
         e.preventDefault();
         try {
             menu.style.left = `${e.pageX}px`;
             menu.style.top = `${e.pageY}px`;
             sourcecode.innerHTML = `<a href="javascript:var code = document.documentElement.innerHTML;document.write('<html></html>');document.body.appendChild(document.createTextNode(code));">View Source</a>`
+            sourcecode.style.display = 'block';
             if(e.target.tagName === "A") {
                 openlink.innerHTML = `<a href="${e.target.href}" target="blank">Open Link</a>`;
-                copylinkoption.innerHTML = `<a href="javascript:navigator.clipboard.writeText('${e.target.href}').then(() => {console.log(new Date + 'Successfully copied link to clipboard.')}).catch(err => {console.error(new Date + ': Error encountered while copying link.\n' + err)})">Copy Link</a>`
+                sharelinkoption.innerHTML = `<a href="javascript:navigator.share({url:'${e.target.href}'});">Share Link</a>`
                 openlink.style.display = 'block';
-                copylinkoption.style.display = 'block';
+                sharelinkoption.style.display = 'block';
             } else {
                 openlink.style.display = 'none';
-                copylink.style.display = 'none';
+                sharelinkoption.style.display = 'none';
             }
             if(e.target.tagName === "IMG" || e.target.tagName === "VIDEO") {
                 rawimgvid.innerHTML = `<a href="${e.target.src}" target="blank">View raw ${e.target.tagName.toLowerCase()}</a>`
                 rawimgvid.style.display = 'block';
             } else {rawimgvid.style.display = 'none';}
             if(window.getSelection().toString().length > 0) {
-                copytext.innerHTML = `<a href="javascript:navigator.clipboard.writeText('${window.getSelection().toString()}').then(() => {console.log(new Date + 'Successfully copied text to clipboard.')}).catch(err => {console.error(new Date + ': Error encountered while copying text.\n' + err)})">Copy Selected Text</a>`
+                copytext.innerHTML = `<a href="javascript:navigator.clipboard.writeText('${window.getSelection().toString()}')">Copy Selected Text</a>`
                 copytext.style.display = 'block';
             } else {copytext.style.display = 'none';}
             menu.style.display = 'block';
